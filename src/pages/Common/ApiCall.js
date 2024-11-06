c626e145374c251c52a037dd7bb7e3cec3bd2570
moonseongmin97 <wja04097@naver.com>
2024-11-06 18:20:26 +0900

    주석 제거

--- src/pages/Common/ApiCall.js
+++ src/pages/Common/ApiCall.js
@@ -10,12 +10,11 @@ async function apiCall({ url, method , payload , onSuccess, onError }) {
                                        headers: {
                                         'Content-Type': 'application/json',
                                         'Accept': 'application/json'
-                                    },  
+                                    },                              
                                     withCredentials: true, // 쿠키를 자동으로 전송하도록 설정 (다른도메인 보안 위험성 확인 필요)
                                        ...(method === 'get' ? {} : { data: payload }) 
-                                    });       
-                        console.log(response.data);                 
-        //if (onSuccess) onSuccess(response.data); // 성공 콜백  
+                                    });                                               
+        if (onSuccess) onSuccess(response.data); // 성공 콜백  
         return { data: response.data, error: null };
     } catch (err) {
         if (onError) onError(err); // 에러 콜백
